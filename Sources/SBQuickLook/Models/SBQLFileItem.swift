94a060e7a20ef7ab04098b36ea88ef19e83b855e
Sebastian Baar <mail.sebastianbaar@gmail.com>
2023-02-24 13:20:31 +0100

    fix documentation

--- Sources/SBQuickLook/Models/SBQLFileItem.swift
+++ Sources/SBQuickLook/Models/SBQLFileItem.swift
@@ -9,9 +9,13 @@ import Foundation
 
 /// Represents the input file item
 public struct SBQLFileItem {
+    /// `URL` of the item, choose between local file URLs or external URLs
     public var url: URL
+    /// Optional title `String` to be displayed in the QuickLook controller.
     public var title: String?
+    /// Optional media type `String` of the file; e.g. `"pdf"`, `"jpeg"`, ...
     public var mediaType: String?
+    /// Optional `URLRequest` used to download the item. The `url` is always set to `fileItem.url`. Default: `URLRequest(url: fileItem.url)`
     public var urlRequest: URLRequest?
     
     /// Initializes the file item with the given values.
@@ -19,7 +23,7 @@ public struct SBQLFileItem {
     /// - Parameters:
     ///   - url: `URL` of the item, choose between local file URLs or external URLs
     ///   - title: Optional title `String` to be displayed in the QuickLook controller.
-    ///   - content: Optional media type `String` of the file; e.g. `"pdf"`, `"jpeg"`, ...
+    ///   - mediaType: Optional media type `String` of the file; e.g. `"pdf"`, `"jpeg"`, ...
     ///   - urlRequest: Optional `URLRequest` used to download the item. The `url` is always set to `fileItem.url`. Default: `URLRequest(url: fileItem.url)`
     public init(url: URL, title: String? = nil, mediaType: String? = nil, urlRequest: URLRequest? = nil) {
         self.url = url
